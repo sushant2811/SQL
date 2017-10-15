022d81e0ddc53f5429bb94374b7cd70df4b79e20
Sushant More <more.13@osu.edu>
2017-10-14 23:13:16 -0400

    Using COUNT

--- SQL101.sql
+++ SQL101.sql
@@ -14,3 +14,14 @@ SELECT DISTINCT country FROM films; /*say want to know which countries are
 represented in the films table*/
 
 SELECT DISTINCT role FROM roles;
+
+/* COUNT -- allows to count number of rows */
+
+SELECT COUNT(*) FROM reviews /*Getting number of records in the reviews table*/
+
+SELECT COUNT(*) FROM people; /*number of rows in the people table*/
+
+SELECT COUNT(birthdate) FROM people; /*number of non-missing birth dates in the 
+people table*/
+
+
