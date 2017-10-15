fe82c50a575b301ddbe2b0cc189ec7c9d6236c1b
Sushant More <more.13@osu.edu>
2017-10-15 13:11:00 -0400

    GROUP BY 202

--- SQL101.sql
+++ SQL101.sql
@@ -296,6 +296,18 @@ SELECT imdb_score, COUNT(*)
 FROM reviews
 GROUP BY imdb_score
 
+/* Get the language and total gross amount films in each language made */
+SELECT language, SUM(gross)
+FROM films
+GROUP BY language;
+
+/* Get the release year, country, and highest budget spent making a film for 
+each year, for each country. Sort your results by release year and country */
+SELECT release_year, country, MAX(budget)
+FROM films
+GROUP BY release_year, country
+ORDER BY release_year, country;
+
 
 
 
