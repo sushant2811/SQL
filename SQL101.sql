fd6c7e73ebbad85cb0af6079896cd4f74fcf2b58
Sushant More <more.13@osu.edu>
2017-10-15 01:46:33 -0400

    Using DESC

--- SQL101.sql
+++ SQL101.sql
@@ -233,7 +233,25 @@ ORDER BY duration;
 and order the results alphabetically */
 SELECT title, gross
 FROM films
-WHERE title LIKE 'M%'
+WHERE title LIKE 'M%';
+
+/* Get the IMDB score and film ID for every film from the reviews table, 
+sorted from highest to lowest score */
+SELECT imdb_score, film_id
+FROM reviews 
+ORDER BY imdb_score DESC;
+
+/* Get the title for every film, in reverse order */
+SELECT title 
+FROM films
+ORDER BY films DESC;
+
+/* Get the title and duration for every film, in order of longest duration 
+to shortest */
+SELECT title, duration
+FROM films
+ORDER BY duration DESC
+
 
 
 
