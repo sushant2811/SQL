a78c43e9b5fa96b4b48861aabeb0ab793c93a196
Sushant More <more.13@osu.edu>
2017-10-15 00:58:07 -0400

    Agregate functions 202

--- SQL101.sql
+++ SQL101.sql
@@ -143,7 +143,7 @@ SELECT name
 FROM people
 WHERE name NOT LIKE 'A%'
 
-/******* Aggregrate functions *************/
+/******* Aggregate functions *************/
 
 /* Use the SUM function to get the total duration of all films */
 SELECT SUM(duration) 
@@ -157,6 +157,27 @@ FROM films;
 SELECT MIN(duration)
 FROM films;
 
+/* get the total amount grossed by all films made in the year 2000 or later */
+SELECT SUM(gross)
+FROM films
+WHERE release_year >= 2000;
+
+/* Get the average amount grossed by all films whose titles start with the 
+letter 'A' */
+SELECT AVG(gross)
+FROM films
+WHERE title LIKE 'A%';
+
+/* Get the amount grossed by the worst performing film in 1994 */
+SELECT MIN(gross)
+FROM films
+WHERE release_year = 1994;
+
+/* Get the amount grossed by the best performing film between 2000 and 2012, 
+inclusive */
+SELECT MAX(gross)
+FROM films
+WHERE release_year BETWEEN 2000 AND 2012;
 
 
 
