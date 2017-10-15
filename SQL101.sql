bfdeca239241d18db051891d58d966eb33538027
Sushant More <more.13@osu.edu>
2017-10-14 23:07:55 -0400

    Using DISTINCT

--- SQL101.sql
+++ SQL101.sql
@@ -7,3 +7,10 @@ SELECT title, release_year FROM films;
 SELECT title, release_year, country FROM films;
 
 SELECT * FROM films;
+
+/* DISTINCT */
+
+SELECT DISTINCT country FROM films; /*say want to know which countries are 
+represented in the films table*/
+
+SELECT DISTINCT role FROM roles;
