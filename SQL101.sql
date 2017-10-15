14385682024c39d9a0dc7c8bafbb26a83043ea66
Sushant More <more.13@osu.edu>
2017-10-15 00:43:16 -0400

    Aggregate functions 101

--- SQL101.sql
+++ SQL101.sql
@@ -143,8 +143,19 @@ SELECT name
 FROM people
 WHERE name NOT LIKE 'A%'
 
+/******* Aggregrate functions *************/
 
+/* Use the SUM function to get the total duration of all films */
+SELECT SUM(duration) 
+FROM films;
 
+/* Get the average duration of all films */
+SELECT AVG(duration)
+FROM films;
+
+/* Get the duration of the shortest film */
+SELECT MIN(duration)
+FROM films;
 
 
 
