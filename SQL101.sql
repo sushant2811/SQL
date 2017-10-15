b76fbb834c6acd2a0a8c70603b0f68d0f71b4cc6
Sushant More <more.13@osu.edu>
2017-10-15 00:13:30 -0400

    Using BETWEEN

--- SQL101.sql
+++ SQL101.sql
@@ -87,6 +87,21 @@ AND gross > 2000000;
 
 /*Build queries one step at a time*/
 
+/* BETWEEN -- remember its inclusive, meaning the beginning and end values are 
+included in the results */
+
+/*get the title and release year of all Spanish language films released between 
+1990 and 2000 (inclusive) with budgets over $100 million*/
+SELECT title, release_year 
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+AND budget > 100000000
+AND (language = 'Spanish' OR language = 'French')
+
+
+
+
+
 
 
 
