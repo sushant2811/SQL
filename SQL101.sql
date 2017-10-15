a569a6309cc1e0d4eba3ca4fe2e2db6bd7acddb5
Sushant More <more.13@osu.edu>
2017-10-14 23:17:31 -0400

    Using COUNT and DISTINCT together

--- SQL101.sql
+++ SQL101.sql
@@ -24,4 +24,9 @@ SELECT COUNT(*) FROM people; /*number of rows in the people table*/
 SELECT COUNT(birthdate) FROM people; /*number of non-missing birth dates in the 
 people table*/
 
+/*Number of unique birthdate*/
+SELECT COUNT(DISTINCT birthdate) FROM people; 
+
+/*Count the number of unique languages in the films table*/
+SELECT COUNT(DISTINCT language) FROM films;
 
