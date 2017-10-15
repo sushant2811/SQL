79be89165b03f7eb41fac676ea4b1bbe3abbab52
Sushant More <more.13@osu.edu>
2017-10-14 23:00:55 -0400

    SQL 101 file added

---
+++ SQL101.sql
@@ -0,0 +1,9 @@
+/* Selecting one and more fields from a table */
+
+SELECT title FROM films;
+
+SELECT title, release_year FROM films;
+
+SELECT title, release_year, country FROM films;
+
+SELECT * FROM films;
