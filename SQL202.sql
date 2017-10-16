44ccb198cf5f66252ab85be170ea82c4ec525c1c
Sushant More <more.13@osu.edu>
2017-10-16 01:29:50 -0400

    Self-join exploration

--- SQL202.sql
+++ SQL202.sql
@@ -75,3 +75,46 @@ SELECT c.name AS country, c.continent, l.name AS language, l.official
 FROM countries AS c
 INNER JOIN languages AS l
 USING (code);
+
+/**** Self-join ***/
+
+/*
+In this exercise, you'll use the populations table to perform a self-join to 
+calculate the percentage increase in population from 2010 to 2015 for each 
+country code!
+*/
+
+/*step 1*/
+
+SELECT p1.country_code, 
+       p1.size AS size2010,
+       p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
+
+/*This produces a result that for each country_code has four entries 
+laying out all combinations of 2010 and 2015*/
+
+/*step 2*/
+SELECT p1.country_code, 
+       p1.size AS size2010,
+       p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code AND p1.year = p2.year - 5
+
+/*The last AND condition removes the three combinations we are not interested 
+in*/
+/*Finally to get the percentage do the following*/
+
+/* step 3*/
+SELECT p1.country_code, 
+       p1.size AS size2010,
+       p2.size AS size2015, 
+       (p2.size - p1.size) / p1.size * 100.0 AS growth_perc 
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code AND p1.year = p2.year - 5
+
+
