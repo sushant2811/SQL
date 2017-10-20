f5d94d82f6f662b8671b60816c0da071760c0a26
Sushant More <more.13@osu.edu>
2017-10-20 12:49:11 -0400

    subquery in Select clause

--- SQL202.sql
+++ SQL202.sql
@@ -554,3 +554,26 @@ WHERE name IN
     FROM countries
     )
 ORDER BY urbanarea_pop DESC    
+
+/*
+Getting same answer INNER JOIN vs. subquery
+*/
+
+SELECT countries.name AS country, COUNT(*) AS cities_num
+FROM cities
+INNER JOIN countries
+ON countries.code = cities.country_code
+GROUP BY country
+ORDER BY cities_num DESC, country
+LIMIT 9;
+
+SELECT countries.name AS country,
+    (SELECT COUNT(*) AS cities_num
+    FROM cities
+    WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num DESC, country
+LIMIT 9
+
+/*The above two codes give the same answer.  Note that the second one doesn't 
+need GROUP BY*/
