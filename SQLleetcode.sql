bf0cf650b8b9cc76d428ae426207958fe06dba14
Sushant More <sushant.2811@gmail.com>
2018-08-12 01:44:47 -0400

    Getting highest value for each department

--- SQLleetcode.sql
+++ SQLleetcode.sql
@@ -397,3 +397,44 @@ INNER JOIN Logs AS l2
 ON (l2.id = l1.id + 1 AND l1.Num = l2.Num)) AS l12
 INNER JOIN Logs AS l3
 ON (l3.id = l12_id + 1 AND l3.Num = l12_Num)
+
+/*
+The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, Max has the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+
+*/
+
+SELECT Department, Employee, Salary
+FROM
+(SELECT Department, Employee, Salary, 
+RANK() OVER (PARTITION BY Department ORDER BY Salary DESC) AS Rank
+FROM
+(SELECT d.Name AS Department, 
+e.Name AS Employee, Salary
+FROM Employee AS e
+INNER JOIN Department AS d
+ON (e.DepartmentId = d.Id)) AS j) AS k
+WHERE Rank = 1;
