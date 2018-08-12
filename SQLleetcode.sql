4cd693effa039f2ab0902eac33045a82c0514e18
Sushant More <sushant.2811@gmail.com>
2018-08-11 23:19:03 -0400

    Swap salary

--- SQLleetcode.sql
+++ SQLleetcode.sql
@@ -259,3 +259,34 @@ DELETE p1 FROM Person p1,
     Person p2
 WHERE
     p1.Email = p2.Email AND p1.Id > p2.Id
+
+
+/*
+Given a table salary, such as the one below, that has m=male and f=female values. Swap all f and m values (i.e., change all f values to m and vice versa) with a single update query and no intermediate temp table.
+For example:
+| id | name | sex | salary |
+|----|------|-----|--------|
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
+After running your query, the above salary table should have the following rows:
+| id | name | sex | salary |
+|----|------|-----|--------|
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
+
+*/
+
+UPDATE salary
+SET sex = CASE WHEN sex = 'f' then 'm'
+               WHEN sex = 'm' then 'f'
+          END
+
+UPDATE salary
+SET sex = CASE WHEN sex = 'f' then 'm'
+               ELSE 'f'
+          END 
+
