6caf273a9e0660769fee0b7c7b28c40f0a01eaa4
Sushant More <sushant.2811@gmail.com>
2018-08-14 22:27:52 -0400

    getting Nth Highest salary in T-SQL

--- SQLleetcode.sql
+++ SQLleetcode.sql
@@ -461,4 +461,36 @@ WHERE
 FROM Employee
 GROUP BY DepartmentId);
 
+---------------------------------------
+/*
+Write a SQL query to get the nth highest salary from the Employee table.
 
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+*/
+
+-- T-SQL solution
+
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+
+DECLARE @M INT;
+SET @M = @N-1;
+    RETURN (
+        /* Write your T-SQL query statement below. */
+        SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC 
+        OFFSET @M ROWS FETCH NEXT 1 ROWS ONLY
+    );
+END
