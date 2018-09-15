365cfc80a6484560357acedef2425ebd0fe5af5b
Sushant More <sushant.2811@gmail.com>
2018-09-15 19:49:14 -0400

    Department top three salaies using dense_rank

--- SQLleetcode.sql
+++ SQLleetcode.sql
@@ -635,3 +635,50 @@ AND ((s1.id = s2.id-1 AND s1.id = s3.id-2)
     OR (s1.id = s2.id+1 AND s1.id = s3.id-1)
     OR (s1.id = s2.id+1 AND s1.id = s3.id+2))
 ORDER BY s1.id
+
+----------------------------------------------------------------------
+
+/*
+Department top three salaries:
+
+The Employee table holds all employees. Every employee has an Id, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who earn the top three salaries in each of the department. For the above tables, your SQL query should return the following rows.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Randy    | 85000  |
+| IT         | Joe      | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+*/
+
+SELECT Department, Employee, Salary
+FROM
+(SELECT d.Name AS Department, e.Name AS Employee, Salary, 
+DENSE_RANK() OVER (PARTITION BY d.Id ORDER BY Salary DESC) AS Rank
+FROM Employee AS e
+INNER JOIN Department AS d
+ON e.DepartmentId = d.Id) AS Joined
+WHERE RANK <= 3
