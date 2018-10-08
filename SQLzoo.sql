60325a6f9424a9bc16942ba09d7846855b827fc4
Sushant More <sushant.2811@gmail.com>
2018-10-07 20:51:55 -0700

    Using select within select (ALL practice as well)

--- SQLzoo.sql
+++ SQLzoo.sql
@@ -258,7 +258,7 @@ FROM booking
 WHERE booking_date BETWEEN '2016-11-25' AND '2016-12-01'
 GROUP BY 1
 
-/*
+/*10.
 How many guests? Show the number of guests in the hotel on the night of 2016-11-21. Include all those who checked in that day or before but not those who have check out on that day or before.*/
 
 SELECT sum(occupants)
@@ -294,3 +294,136 @@ WHERE booking_id IN
 (SELECT booking_id
 FROM booking
 WHERE booking_date <= '2016-11-21' AND DATEDIFF('2016-11-21', booking_date) < nights) 
+
+/*
+11. Coincidence. Have two guests with the same surname ever stayed in the hotel on the evening? Show the last name and both first names. Do not include duplicates.
+*/
+
+SELECT DISTINCT a.last_name, a.first_name, b.first_name 
+FROM
+(SELECT * 
+FROM booking 
+JOIN guest
+ON guest.id = booking.guest_id) AS a
+JOIN (SELECT * 
+FROM booking 
+JOIN guest
+ON guest.id = booking.guest_id) AS b
+ON (a.last_name = b.last_name AND a.first_name > b.first_name)
+WHERE a.booking_date <= b.booking_date 
+AND DATEDIFF(b.booking_date, a.booking_date) < a.nights
+ORDER BY 1
+
+/*using a.first_name > b.first_name instead of just a.first_name <> b.first_name makes sure we exclude the duplicates*/
+
+/*
+12. Check out per floor. The first digit of the room number indicates the floor – e.g. room 201 is on the 2nd floor. For each day of the week beginning 2016-11-14 show how many guests are checking out that day by floor number. Columns should be day (Monday, Tuesday ...), floor 1, floor 2, floor 3.
+*/
+
+SELECT DATE_ADD(booking_date, INTERVAL nights DAY) AS i, 
+SUM(CASE WHEN room_no LIKE '1%' THEN 1 ELSE 0 END) AS '1st',
+SUM(CASE WHEN room_no LIKE '2%' THEN 1 ELSE 0 END) AS '2nd',
+SUM(CASE WHEN room_no LIKE '3%' THEN 1 ELSE 0 END) AS '3rd'
+FROM booking 
+WHERE DATE_ADD(booking_date, INTERVAL nights DAY) BETWEEN '2016-11-14' AND '2016-11-20'
+GROUP BY 1
+
+
+/*-------------------------------------------------
+---------------USING SELECT WITHIN SELECT----------
+------------------------------------------------*/
+
+/*Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.*/
+
+SELECT name 
+FROM world 
+WHERE continent = 'Europe' 
+AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom')
+
+/*List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+*/
+
+SELECT name, continent 
+FROM world
+WHERE continent IN (SELECT continent FROM world WHERE name IN ('Argentina', 'Australia'))
+ORDER BY 1;
+
+/*
+Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+*/
+
+SELECT name, population
+FROM world
+WHERE population > (SELECT population FROM world WHERE name = 'Canada') 
+AND population < (SELECT population FROM world WHERE name = 'Poland');
+
+/*5.
+Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+
+Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.*/ 
+
+SELECT name, CONCAT(ROUND(population * 100 / (SELECT population FROM world WHERE name = 'Germany'),0), '%') 
+FROM world
+WHERE continent = 'Europe';
+
+/*
+6.
+Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)*/
+
+SELECT name 
+FROM world 
+WHERE gdp > ALL(SELECT IFNULL(gdp,0) FROM world WHERE continent = 'Europe');
+
+/*7.
+Find the largest country (by area) in each continent, show the continent, the name and the area:*/
+
+SELECT continent, name, area
+FROM world AS w1
+WHERE area >= ALL (SELECT area FROM world AS w2 
+WHERE w1.continent = w2.continent);
+
+/*
+The above example is known as a correlated or synchronized sub-query.
+
+Using correlated subqueries
+A correlated subquery works like a nested loop: the subquery only has access to rows related to a single record at a time in the outer query. The technique relies on table aliases to identify two different uses of the same table, one in the outer query and the other in the subquery.
+
+One way to interpret the line in the WHERE clause that references the two table is “… where the correlated values are the same”.
+
+In the example provided, you would say “select the country details from world where the population is greater than or equal to the population of all countries where the continent is the same”.
+*/
+
+/*
+8.
+List each continent and the name of the country that comes first alphabetically
+*/
+
+SELECT continent, min(name)
+FROM world 
+GROUP BY 1;
+
+SELECT continent, name
+FROM world AS w1
+WHERE name <= ALL (SELECT name FROM world AS w2 WHERE w1.continent = w2.continent);
+
+/*
+9.
+Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.*/
+
+SELECT name, continent, population 
+FROM world
+WHERE continent IN 
+(SELECT continent FROM world 
+GROUP BY continent HAVING max(population) <= 25000000);
+
+SELECT name, continent, population 
+FROM world AS w1
+WHERE 25000000 > ALL (SELECT population FROM world w2 WHERE w1.continent = w2.continent);
+
+/*10.
+Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.*/   
+
+SELECT name, continent 
+FROM world w1
+WHERE population >  ALL (SELECT 3.0 * population FROM world w2 
+WHERE w1.continent = w2.continent AND w1.name <> w2.name);
